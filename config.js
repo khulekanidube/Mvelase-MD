d3aafe880bde680c223fb78f3ec5c2356ff58040
khulekanidube <khulekanimvelase725@gmail.com>
2025-01-26 13:48:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "RkxwiJBD#1yirxrzzRH44F4vhw_glbk91qIQw9k_0PVvYTqHzNTc",
 // add your Session Id 
 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
